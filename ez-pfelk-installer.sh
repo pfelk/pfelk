c39710fdedc909b3da8befc6728a4aa1da11bf18
revere521 <revere521@hotmail.com>
2020-04-22 23:00:09 -0400

    Add Debian 10.3 support and 35-rules-desc.conf
    the URL for the deb file will need to be checked and updated periodically (https://github.com/maxmind/geoipupdate/releases/download/v4.3.0/geoipupdate_4.3.0_linux_amd64.deb)

--- ez-pfelk-installer.sh
+++ ez-pfelk-installer.sh
@@ -15,6 +15,8 @@ command -v sudo >/dev/null 2>&1 || { echo >&2 "pfELK: sudo is required to be ins
 
 # Installing pfELK dependencies
 echo "pfELK: Installing Inital Dependencies & Added Respositories"
+
+#Redhat
 if [ -f "/etc/redhat-release" ] || [ -f "/etc/system-release" ]; then
   sudo yum -y install wget apt-transport-https; sudo yum add-apt-repository ppa:maxmind/ppa; sudo echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list; sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
   if [ $? -ne 0 ]; then
@@ -23,7 +25,17 @@ if [ -f "/etc/redhat-release" ] || [ -f "/etc/system-release" ]; then
   fi
 fi
 
-if [ -f "/etc/debian_version" ]; then
+#Debian
+if hostnamectl | grep 'Operating System: Debian*' > /dev/null; then
+  sudo apt-get -qq install wget apt-transport-https gnupg2 software-properties-common dirmngr lsb-release ca-certificates; sudo echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list; sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+  if [ $? -ne 0 ]; then
+    echo "pfELK: apt-get failed"
+    exit 1
+  fi
+fi
+
+#Ubuntu
+if hostnamectl | grep 'Operating System: Ubuntu*' > /dev/null; then
   sudo apt-get -qq install wget apt-transport-https; sudo add-apt-repository ppa:maxmind/ppa; sudo echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list; sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
   if [ $? -ne 0 ]; then
     echo "pfELK: apt-get failed"
@@ -31,13 +43,16 @@ if [ -f "/etc/debian_version" ]; then
   fi
 fi
 
+#FreeBSD (forthcoming)
 #if [ "$UNAME" = "FreeBSD" ]; then
 #  sudo pkg_add -Fr wget apt-transport-https; sudo add-apt-repository ppa:maxmind/ppa; sudo echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list; sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 #  MAKE=gmake
 #fi
 
+
 # Update Required Respositories
 echo "pfELK: Updating Respositories"
+#Redhat
 if [ -f "/etc/redhat-release" ] || [ -f "/etc/system-release" ]; then
   sudo yum -y update
   if [ $? -ne 0 ]; then
@@ -46,7 +61,17 @@ if [ -f "/etc/redhat-release" ] || [ -f "/etc/system-release" ]; then
   fi
 fi
 
-if [ -f "/etc/debian_version" ]; then
+#Debian
+if hostnamectl | grep 'Operating System: Debian*' > /dev/null; then
+  sudo apt-get -qq update
+  if [ $? -ne 0 ]; then
+    echo "pfELK: apt-get failed"
+    exit 1
+  fi
+fi
+
+#Ubuntu
+if hostnamectl | grep 'Operating System: Ubuntu*' > /dev/null; then
   sudo apt-get -qq update 
   if [ $? -ne 0 ]; then
     echo "pfELK: apt-get failed"
@@ -54,13 +79,16 @@ if [ -f "/etc/debian_version" ]; then
   fi
 fi
 
+#FreeBSD (forthcoming)
 #if [ "$UNAME" = "FreeBSD" ]; then
 #  sudo pkg_add -Fr update
 #  MAKE=gmake
 #fi
 
 # Install Required Respositories
-echo "pfELK: Installing Required Respositories"
+echo "pfELK: Installing Required Respositories - This may take some time, Please be patient"
+
+#Redhat
 if [ -f "/etc/redhat-release" ] || [ -f "/etc/system-release" ]; then
   sudo yum -y install openjdk-11-jre; sudo apt-get -qq install geoipupdate; sudo apt-get -qq install elasticsearch; sudo apt-get -qq install kibana; sudo apt-get -qq install logstash
   if [ $? -ne 0 ]; then
@@ -69,14 +97,26 @@ if [ -f "/etc/redhat-release" ] || [ -f "/etc/system-release" ]; then
   fi
 fi
 
-if [ -f "/etc/debian_version" ]; then
-  sudo apt -qq install openjdk-11-jre; sudo apt-get -qq install geoipupdate; sudo apt-get -qq install elasticsearch; sudo apt-get -qq install kibana; sudo apt-get -qq install logstash
+#Debian
+if hostnamectl | grep 'Operating System: Debian*' > /dev/null; then
+  sudo apt-get -qq install openjdk-11-jre; sudo apt-get -qq install elasticsearch; sudo apt-get -qq install kibana; sudo apt-get -qq install logstash; cd $HOME; wget https://github.com/maxmind/geoipupdate/releases/download/v4.3.0/geoipupdate_4.3.0_linux_amd64.deb; sudo dpkg -i ./geoipupdate_4.3.0_linux_amd64.deb
   if [ $? -ne 0 ]; then
     echo "pfELK: apt-get failed"
     exit 1
   fi
 fi
 
+#Ubuntu
+if hostnamectl | grep 'Operating System: Ubuntu*' > /dev/null; then
+  sudo apt-get -qq install openjdk-11-jre; sudo apt-get -qq install geoipupdate; sudo apt-get -qq install elasticsearch; sudo apt-get -qq install kibana; sudo apt-get -qq install logstash
+  if [ $? -ne 0 ]; then
+    echo "pfELK: apt-get failed"
+    exit 1
+  fi
+fi
+
+
+#FreeBSD (forthcoming)
 #if [ "$UNAME" = "FreeBSD" ]; then
 #  sudo pkg_add -Fr install openjdk-11-jre; sudo apt-get -qq install geoipupdate; sudo apt-get -qq install elasticsearch; sudo apt-get -qq install kibana; sudo apt-get -qq install logstash
 #  MAKE=gmake
@@ -97,6 +137,7 @@ sudo swapoff -a
   sudo wget https://raw.githubusercontent.com/3ilson/pfelk/master/conf.d/20-suricata.conf
   sudo wget https://raw.githubusercontent.com/3ilson/pfelk/master/conf.d/25-snort.conf
   sudo wget https://raw.githubusercontent.com/3ilson/pfelk/master/conf.d/30-geoip.conf
+  sudo wget https://raw.githubusercontent.com/3ilson/pfelk/master/conf.d/35-rules-desc.conf
   sudo wget https://raw.githubusercontent.com/3ilson/pfelk/master/conf.d/40-dns.conf
   sudo wget https://raw.githubusercontent.com/3ilson/pfelk/master/conf.d/45-cleanup.conf
   sudo wget https://raw.githubusercontent.com/3ilson/pfelk/master/conf.d/50-outputs.conf
@@ -105,9 +146,11 @@ sudo swapoff -a
   cd /etc/logstash/conf.d/template
   sudo wget https://raw.githubusercontent.com/3ilson/pfelk/master/conf.d/template/pf-geoip-template.json
 
+##
 # Add Configuration Script Here
+##
 
-# Install/Troubleshoot Success Message 
+# Install/Troubleshoot Success Message
   sudo mkdir /etc/pfelk
   cd /etc/pfelk
   sudo wget https://raw.githubusercontent.com/3ilson/pfelk/master/readme.txt
