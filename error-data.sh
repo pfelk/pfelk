b41af37abb93cac3ab72d0ebfa493303e7ae80d6
Andrew <a@3ilson.com>
2020-04-25 10:05:07 -0400

    Update error-data.sh

--- error-data.sh
+++ error-data.sh
@@ -4,6 +4,6 @@ find /data/ | cat >> /data/pfELK/error.pfelk
 cat /data/pfELK/filters/*.conf >> /data/pfELK/error.pfelk 
 tail -20 /data/pfELK/logs/logstash-plain.log | cat >> /data/pfELK/test.pfelk
 java -version | cat >> /data/pfELK/test.pfelk
-systemctl status elasticsearch.service -q | cat >> /data/pfELK/error.pfelk
+journalctl -u elasticsearch -n 50 | cat >> /data/pfELK/error.pfelk
 systemctl status logstash.service -q | cat >> /data/pfELK/error.pfelk
 systemctl status kibana.service -q | cat >> /data/pfELK/error.pfelk
