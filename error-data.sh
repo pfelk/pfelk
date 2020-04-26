70b0746600d4c4391fe7637837f0304fdc84fed0
revere521 <revere521@hotmail.com>
2020-04-26 11:08:54 -0400

    Update error-data.sh
    github like the .log file extension for uploads

--- error-data.sh
+++ error-data.sh
@@ -1,42 +1,42 @@
 echo "pfELK: Generating pfELK Error Data"
 #remove any old pfelk error outputs
-sudo rm /data/pfELK/error.pfelk
+sudo rm /data/pfELK/error.pfelk.log
 #create the new file
-sudo touch /data/pfELK/error.pfelk
+sudo touch /data/pfELK/error.pfelk.log
 #add system information
-echo "#####################################" >> /data/pfELK/error.pfelk
-echo "# pfELK System Information ##########" >> /data/pfELK/error.pfelk
-echo "#####################################\n" >> /data/pfELK/error.pfelk
-printf "$(uname -srm)\n$(cat /etc/os-release)\n" | cat >> /data/pfELK/error.pfelk
+echo "#####################################" >> /data/pfELK/error.pfelk.log
+echo "# pfELK System Information ##########" >> /data/pfELK/error.pfelk.log
+echo "#####################################\n" >> /data/pfELK/error.pfelk.log
+printf "$(uname -srm)\n$(cat /etc/os-release)\n" | cat >> /data/pfELK/error.pfelk.log
 #capture directory and files structure
-echo "\n#####################################" >> /data/pfELK/error.pfelk
-echo "# Listing pfELK Directory Structure #" >> /data/pfELK/error.pfelk
-echo "#####################################\n" >> /data/pfELK/error.pfelk
+echo "\n#####################################" >> /data/pfELK/error.pfelk.log
+echo "# Listing pfELK Directory Structure #" >> /data/pfELK/error.pfelk.log
+echo "#####################################\n" >> /data/pfELK/error.pfelk.log
 find /data/ | cat >> /data/pfELK/error.pfelk
 #capture all config files
-echo "\n#####################################" >> /data/pfELK/error.pfelk
-echo "# pfELK Config File Details #########" >> /data/pfELK/error.pfelk
-echo "#####################################\n" >> /data/pfELK/error.pfelk
-cat /data/pfELK/configurations/*.conf >> /data/pfELK/error.pfelk
+echo "\n#####################################" >> /data/pfELK/error.pfelk.log
+echo "# pfELK Config File Details #########" >> /data/pfELK/error.pfelk.log
+echo "#####################################\n" >> /data/pfELK/error.pfelk.log
+cat /data/pfELK/configurations/*.conf >> /data/pfELK/error.pfelk.log
 #attach logstash logs
-echo "\n#####################################" >> /data/pfELK/error.pfelk
-echo "# Appending Logstash Logs ###########" >> /data/pfELK/error.pfelk
-echo "#####################################\n" >> /data/pfELK/error.pfelk
-tail -20 /data/pfELK/logs/logstash-plain.log | cat >> /data/pfELK/error.pfelk
+echo "\n#####################################" >> /data/pfELK/error.pfelk.log
+echo "# Appending Logstash Logs ###########" >> /data/pfELK/error.pfelk.log
+echo "#####################################\n" >> /data/pfELK/error.pfelk.log
+tail -20 /data/pfELK/logs/logstash-plain.log | cat >> /data/pfELK/error.pfelk.log
 #attach Java version
-echo "\n#####################################" >> /data/pfELK/error.pfelk
-echo "# Java Version ######################" >> /data/pfELK/error.pfelk
-echo "#####################################\n" >> /data/pfELK/error.pfelk
-java -version 2>> /data/pfELK/error.pfelk
+echo "\n#####################################" >> /data/pfELK/error.pfelk.log
+echo "# Java Version ######################" >> /data/pfELK/error.pfelk.log
+echo "#####################################\n" >> /data/pfELK/error.pfelk.log
+java -version 2>> /data/pfELK/error.pfelk.log
 #capture systemctl status outputs to validate services running
-echo "\n#####################################" >> /data/pfELK/error.pfelk
-echo "# ELK Services Check ################" >> /data/pfELK/error.pfelk
-echo "#####################################\n" >> /data/pfELK/error.pfelk
-echo "\n###Elasticsearch.service:###\n" >> /data/pfELK/error.pfelk
-systemctl status elasticsearch.service -q | cat >> /data/pfELK/error.pfelk
-echo "\n###Logstash.service:###\n" >> /data/pfELK/error.pfelk
-systemctl status logstash.service -q | cat >> /data/pfELK/error.pfelk
-echo "\n###Kibana.service:###\n" >> /data/pfELK/error.pfelk
-systemctl status kibana.service -q | cat >> /data/pfELK/error.pfelk
+echo "\n#####################################" >> /data/pfELK/error.pfelk.log
+echo "# ELK Services Check ################" >> /data/pfELK/error.pfelk.log
+echo "#####################################\n" >> /data/pfELK/error.pfelk.log
+echo "\n###Elasticsearch.service:###\n" >> /data/pfELK/error.pfelk.log
+systemctl status elasticsearch.service -q | cat >> /data/pfELK/error.pfelk.log
+echo "\n###Logstash.service:###\n" >> /data/pfELK/error.pfelk.log
+systemctl status logstash.service -q | cat >> /data/pfELK/error.pfelk.log
+echo "\n###Kibana.service:###\n" >> /data/pfELK/error.pfelk.log
+systemctl status kibana.service -q | cat >> /data/pfELK/error.pfelk.log
 echo "Error Data Collected Successfully"
-echo "Attach the contents of /data/pfELK/error.pfelk as a file to your Issue in github"
+echo "Attach the contents of /data/pfELK/error.pfelk.log as a file to your Issue in github"
