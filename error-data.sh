0c11e3e59963b72c5955510b78d16124fca73185
revere521 <revere521@hotmail.com>
2020-04-26 09:49:21 -0400

    Update error-data.sh
    changed /data/pfELK/filters/*.conf to /data/pfELK/configurations/*.conf in line 4

--- error-data.sh
+++ error-data.sh
@@ -1,7 +1,7 @@
 echo "pfELK: Generating pfELK Error Data"
 printf "$(uname -srm)\n$(cat /etc/os-release)\n" | cat >> /data/pfELK/error.pfelk
 find /data/ | cat >> /data/pfELK/error.pfelk
-cat /data/pfELK/filters/*.conf >> /data/pfELK/error.pfelk 
+cat /data/pfELK/configurations/*.conf >> /data/pfELK/error.pfelk 
 tail -20 /data/pfELK/logs/logstash-plain.log | cat >> /data/pfELK/test.pfelk
 java -version | cat >> /data/pfELK/test.pfelk
 journalctl -u elasticsearch -n 50 | cat >> /data/pfELK/error.pfelk
