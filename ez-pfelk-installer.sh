6d9472340addb0b491ffd3a0678b993981a084af
Andrew <a@3ilson.com>
2020-05-24 18:57:56 -0400

    Update ez-pfelk-installer.sh

--- ez-pfelk-installer.sh
+++ ez-pfelk-installer.sh
@@ -156,7 +156,7 @@ sudo swapoff -a
 
 # Install/Troubleshoot Success Message
 echo "pfELK: Finalizing Installtion"
-  echo 00 17 * * 0 geoipupdate -d /data/pfELK/GeoIP > /etc/cron.weekly/geoipupdate
+  echo "00 17 * * 0 geoipupdate -d /data/pfELK/GeoIP" > /etc/cron.weekly/geoipupdate
   sed -i 's/EditionIDs GeoLite2-Country GeoLite2-City/EditionIDs GeoLite2-Country GeoLite2-City GeoLite2-ASN/g' /etc/GeoIP.conf
   cd /etc/logstash
   sudo wget https://raw.githubusercontent.com/3ilson/pfelk/master/pipelines.yml
