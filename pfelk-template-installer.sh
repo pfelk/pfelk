f145e258d4b866296e3dfc44c4c7f4001d581bc5
Andrew <a@3ilson.com>
2021-01-03 11:14:33 -0500

    Update pfelk-template-installer.sh
    Updated haproxy.timestamp to recognize unique time/date format
    Issue #219

--- pfelk-template-installer.sh
+++ pfelk-template-installer.sh
@@ -3957,82 +3957,62 @@ curl -X PUT "localhost:9200/_index_template/pfelk-haproxy?pretty" -H 'Content-Ty
   "priority": 90,
   "template": {
     "mappings": {
-      "_routing": {
-        "required": false
-      },
-      "numeric_detection": false,
-      "dynamic_date_formats": [
-        "strict_date_optional_time",
-        "yyyy/MM/dd HH:mm:ss Z||yyyy/MM/dd Z",
-        "dd/MMM/yyyy:HH:mm:ss.SSS"
-      ],
-      "dynamic": true,
       "_source": {
         "excludes": [],
         "includes": [],
         "enabled": true
       },
-      "date_detection": true,
+      "_routing": {
+        "required": false
+      },
+      "dynamic": true,
+      "numeric_detection": false,
+      "date_detection": false,
+      "dynamic_templates": [],
       "properties": {
         "haproxy": {
           "type": "object",
           "properties": {
-            "server_name": {
-              "eager_global_ordinals": false,
-              "norms": false,
-              "index": true,
-              "store": false,
-              "type": "keyword",
-              "fields": {
-                "text": {
-                  "type": "text"
-                }
-              },
-              "index_options": "docs",
-              "split_queries_on_whitespace": false,
-              "doc_values": true
-            },
-            "termination_state": {
-              "eager_global_ordinals": false,
-              "norms": false,
-              "index": true,
-              "store": false,
-              "type": "keyword",
-              "fields": {
-                "text": {
-                  "type": "text"
-                }
-              },
-              "index_options": "docs",
-              "split_queries_on_whitespace": false,
-              "doc_values": true
-            },
-            "time_queue": {
-              "type": "long"
-            },
-            "bytes_read": {
-              "type": "long"
-            },
-            "mode": {
-              "type": "keyword"
-            },
-            "backend_queue": {
-              "type": "long"
-            },
             "backend_name": {
               "eager_global_ordinals": false,
               "norms": false,
               "index": true,
               "store": false,
               "type": "keyword",
+              "index_options": "docs",
+              "split_queries_on_whitespace": false,
+              "doc_values": true,
               "fields": {
                 "text": {
                   "type": "text"
                 }
-              },
-              "index_options": "docs",
-              "split_queries_on_whitespace": false,
-              "doc_values": true
+              }
+            },
+            "backend_queue": {
+              "type": "long"
+            },
+            "bytes_read": {
+              "type": "long"
+            },
+            "connections": {
+              "type": "object",
+              "properties": {
+                "active": {
+                  "type": "long"
+                },
+                "backend": {
+                  "type": "long"
+                },
+                "frontend": {
+                  "type": "long"
+                },
+                "retries": {
+                  "type": "long"
+                },
+                "server": {
+                  "type": "long"
+                }
+              }
             },
             "frontend_name": {
               "eager_global_ordinals": false,
@@ -4040,14 +4020,14 @@ curl -X PUT "localhost:9200/_index_template/pfelk-haproxy?pretty" -H 'Content-Ty
               "index": true,
               "store": false,
               "type": "keyword",
+              "index_options": "docs",
+              "split_queries_on_whitespace": false,
+              "doc_values": true,
               "fields": {
                 "text": {
                   "type": "text"
                 }
-              },
-              "index_options": "docs",
-              "split_queries_on_whitespace": false,
-              "doc_values": true
+              }
             },
             "http": {
               "type": "object",
@@ -4080,34 +4060,55 @@ curl -X PUT "localhost:9200/_index_template/pfelk-haproxy?pretty" -H 'Content-Ty
                 }
               }
             },
+            "mode": {
+              "type": "keyword"
+            },
+            "server_name": {
+              "eager_global_ordinals": false,
+              "norms": false,
+              "index": true,
+              "store": false,
+              "type": "keyword",
+              "index_options": "docs",
+              "split_queries_on_whitespace": false,
+              "doc_values": true,
+              "fields": {
+                "text": {
+                  "type": "text"
+                }
+              }
+            },
             "server_queue": {
               "type": "long"
+            },
+            "termination_state": {
+              "eager_global_ordinals": false,
+              "norms": false,
+              "index": true,
+              "store": false,
+              "type": "keyword",
+              "index_options": "docs",
+              "split_queries_on_whitespace": false,
+              "doc_values": true,
+              "fields": {
+                "text": {
+                  "type": "text"
+                }
+              }
             },
             "time_backend_connect": {
               "type": "long"
             },
-            "connections": {
-              "type": "object",
-              "properties": {
-                "retries": {
-                  "type": "long"
-                },
-                "server": {
-                  "type": "long"
-                },
-                "active": {
-                  "type": "long"
-                },
-                "backend": {
-                  "type": "long"
-                },
-                "frontend": {
-                  "type": "long"
-                }
-              }
+            "time_queue": {
+              "type": "long"
             },
             "timestamp": {
-              "type": "date"
+              "format": "strict_date_optional_time||epoch_millis||dd/MMM/yyyy:HH:mm:ss.SSS",
+              "index": true,
+              "ignore_malformed": false,
+              "store": false,
+              "type": "date",
+              "doc_values": true
             }
           }
         }
