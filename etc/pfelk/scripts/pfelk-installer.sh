10338be4ec8c9a20f32b51407dfed9a6fac3d622
a3ilson <613377@proton.me>
2025-06-22 17:49:31 -0400

    Update pfelk-installer.sh

--- etc/pfelk/scripts/pfelk-installer.sh
+++ etc/pfelk/scripts/pfelk-installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version    | 25.01
+# Version    | 25.06
 # Website    | https://github.com/pfelk/pfelk
 ########################################################
 #pfELK Installation Script                             #
@@ -18,7 +18,7 @@
 # MaxMind      | https://github.com/maxmind/geoipupdate/releases
 # GeoIP        | 7.1.0
 # Elastic      | https://www.elastic.co/guide/en/elasticsearch/reference/current/es-release-notes.html
-# Elasticstack | 8.17.1
+# Elasticstack | 9.0.0
 ########################################################
 #Color Codes                                           #
 ########################################################
@@ -421,7 +421,7 @@ port_5140_service=''
 port_5040_in_use=''
 port_5040_pid=''
 port_5040_service=''
-elk_version=8.17.1
+elk_version=9.0.0
 maxmind_version=7.1.0
 ########################################################
 #Required Packages                                     #
